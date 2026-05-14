1e1402d444d79f7635b7f2500efb4a07a991721f
Renaud Paulin <paulin.renaud@etu.iut-tlse3.fr>
2026-05-14 11:28:28 +0200

    Programme.java

--- Projet Java/ProjetCPU/src/main/java/assembleur/Programme.java
+++ Projet Java/ProjetCPU/src/main/java/assembleur/Programme.java
@@ -1,5 +1,43 @@
 package assembleur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import instructions.Instruction;
+
 public class Programme {
-    
+
+    private String codeSource;
+    private boolean estAssemble;
+    private List<Instruction> instructions;
+
+    public Programme(String codeSource) {
+        this.codeSource = codeSource;
+        this.estAssemble = false;
+        this.instructions = new ArrayList<>();
+    }
+
+    public String getCodeSource() {
+        return codeSource;
+    }
+
+    public void ajouterInstruction(Instruction inst) {
+        instructions.add(inst);
+    }
+
+    public Instruction getInstruction(int index) {
+        return instructions.get(index);
+    }
+
+    public int nombreInstructions() {
+        return instructions.size();
+    }
+
+    public void marquerAssemble() {
+        this.estAssemble = true;
+    }
+
+    public boolean estAssemble() {
+        return estAssemble;
+    }
 }
